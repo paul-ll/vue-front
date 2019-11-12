54f5fd15944a261e378a436de7e32a0737f979ce
lvlei <lvlei@missfresh.cn>
2019-11-12 12:08:27 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <ul>
       <li v-for="user in users" :key="user.id">
-       {{user.id}} :{{user.name}}
+       {{user.id}} :{{user.name}} :{{user.id}}
       </li>
     </ul>
   </div>
