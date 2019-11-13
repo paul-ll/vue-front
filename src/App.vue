942d7bd22b891af1fa104cda007d5538fced2c1e
lvlei <lvlei@missfresh.cn>
2019-11-13 11:22:25 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,69 @@
 <template>
   <div id="app">
-    <ul>
-      <li v-for="user in users" :key="user.id">
-      {{user.name}}
-      </li>
-    </ul>
+    <v-loading :text="loadingText" v-if="isLoading"/>
+    <v-headers/>
+    <router-view class="router-views"/>
+    <main-footer/>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+  import {mapGetters} from 'vuex';
+  import VLoading from './components/Loading'
+  import VHeaders from './components/Headers'
+  import MainFooter from './components/MainFooter'
 
-export default {
-  name: 'app',
-  data(){
-    return {
-      users:[]
-    }
-  },
-  mounted(){
-    axios.get('/api/users').then(response=>{
-      this.users = response.data;
-    })
+  export default {
+    components: {
+      VLoading,
+      VHeaders,
+      MainFooter
+    },
+    data() {
+      return {
+        centerDialogVisible: true,
+        showClose: true
+      }
+    },
+    computed: {
+      ...mapGetters('loading', [
+        'isLoading',
+        'loadingText'
+      ])
+    },
+    methods: {}
   }
-
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "./assets/fonts.css";
+  
+  html, body {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    background: #f8f8f8;
+  }
+
+  #app * {
+    font-family: 'Alibaba-PuHuiTi-Regular' !important;
+  }
+
+  #app [class*=" el-icon-"], #app [class^=el-icon-] {
+    font-family: element-icons!important
+  }
+  section, article, header, nav, ul, li, h1, h2, h3, p {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
 </style>
---
+++ src/components/MainFooter.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="Footer-wrap">
+    <footer class="footer-inner">
+      <p>暂定</p>
+    </footer>
+  </section>
+</template>
+
+<script>
+  import {mapState, mapActions} from 'vuex';
+
+  export default {
+    data() {
+      return {}
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .footer-inner {
+    padding: 64px 32px;
+    line-height: 36px;
+    text-align: center;
+    font-size: 18px;
+    color: #fff;
+    background: #262626;
+  }
+
+  .footer-inner a {
+    color: #fff;
+    text-decoration: none;
+  }
+</style>
