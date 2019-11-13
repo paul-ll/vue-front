3eeaeea579326293075338687e12d476ea3d605b
lvlei <lvlei@missfresh.cn>
2019-11-13 13:01:09 +0800

    fix

--- src/libs/util.js
+++ src/libs/util.js
@@ -6,7 +6,8 @@ const ajaxUrl = process.env.NODE_ENV === 'development'
   // 测试环境api接口
   ? 'http://localhost:3000/v1'
   // 线上环境api接口
-  : 'http://95.163.203.224:3000/v1';
+  // : 'http://api.boblog.com/v1';
+  : 'http://95.163.203.224:3000';
 
 util.ajax = axios.create({
   baseURL: ajaxUrl,
