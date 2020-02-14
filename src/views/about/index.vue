9a15ea5d0e8cebf038a12621ed8e1636dd32fc4c
paul-ll <lvl8961@163.com>
2020-02-14 11:27:01 +0800

    FIX

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -8,7 +8,7 @@
     <article class="text">
       <p>
         我叫吕磊，职业是一名前端工程师，这个博客属于我的个人博客网站，同时也在 <a href="https://github.com/paul-ll" target="_blank">
-        Github </a>开源了此项目代码。
+        Github</a>开源了此项目代码。
       </p>
       <p class="content">博客的内容：积极向上的文章。</p>
       <p>博客的愿景：和更多的朋友一起学习进步。</p>
